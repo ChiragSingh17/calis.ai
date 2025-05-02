4cf6394437e4a0dcd4232929ec6b5d967cf2fcbf
ChiragSingh17 <151609196+ChiragSingh17@users.noreply.github.com>
2025-05-02 08:20:03 +0530

    Update Calisthenics.py

--- backend/Calisthenics.py
+++ backend/Calisthenics.py
@@ -30,9 +30,14 @@ cooldown = 2
 
 cap = cv2.VideoCapture(0)
 
+# Timer variables
+start_time = time.time()
+correct_pose_start_time = None
+total_correct_time = 0
+
 #---------------------------------------------------------------------------------------------------
 
-def lsit(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose):
+def lsit(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time):
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -70,19 +75,28 @@ def lsit(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_
                 leg_angle_r = calculate_angle(r_hip, r_knee, r_ankle)
 
                 if not 170 < head_angle_l < 190 and not 170 < head_angle_r < 190:
-                    pose_status = "keep your head straight"
+                    pose_status = "Keep your head straight"
                 elif not 80 < back_angle_l < 100 and not 80 < back_angle_r < 100:
-                    pose_status = "keep your back straight"
+                    pose_status = "Keep your back straight"
                 elif not 170 < leg_angle_l < 195 and not 170 < leg_angle_r < 190:
-                    pose_status = "keep your legs straight"
+                    pose_status = "Keep your legs straight"
                 else:
                     pose_status = "Perfect form ! Keep it up!"
 
                 current_time = time.time()
                 if pose_status != previous_pose and (current_time - last_spoken_time) > cooldown:
                     threading.Thread(target=speak, args=(pose_status,), daemon=True).start()
+                    print("Feedback : ", pose_status)
                     previous_pose = pose_status
                     last_spoken_time = current_time
+
+                if "Perfect" in pose_status:
+                    if correct_pose_start_time is None:
+                        correct_pose_start_time = current_time
+                else:
+                    if correct_pose_start_time is not None:
+                        total_correct_time += current_time - correct_pose_start_time
+                        correct_pose_start_time = None
 
                 all_angles = [head_angle_l, back_angle_l, leg_angle_l, head_angle_r, back_angle_r, leg_angle_r]
                 ideal_angles = [180, 90, 180, 180, 90, 180]
@@ -101,6 +115,17 @@ def lsit(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_
                 cv2.putText(image, f"Accuracy: {avg_accuracy:.2f}%", (w - 250, 50),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 215, 0), 2)
 
+                elapsed_time = int(time.time() - start_time)
+                correct_time_display = int(total_correct_time) if correct_pose_start_time is None else int(total_correct_time + (time.time() - correct_pose_start_time))
+
+                cv2.putText(image, f"Total Time: {elapsed_time}s", (10, h - 80),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+                cv2.putText(image, f"Correct Time: {correct_time_display}s", (10, h - 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+                cv2.putText(image, pose_status, (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                         mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=2),
                                         mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2))
@@ -108,10 +133,16 @@ def lsit(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_
             cv2.imshow("Pose Feedback", image)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    print(f"Total time: {int(time.time() - start_time)} seconds")
+    print(f"Total time with correct pose: {int(total_correct_time) if correct_pose_start_time is None else int(total_correct_time + (time.time() - correct_pose_start_time))} seconds")
+
         
 # ---------------------------------------------------------------------------------------------------
 
-def handstand(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose):
+def handstand(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time):
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -153,19 +184,28 @@ def handstand(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose
                 leg_angle_r = calculate_angle(r_hip, r_knee, r_ankle)
 
                 if not 175 < hand_angle_l < 185 and not 175 < hand_angle_r < 185:
-                    pose_status = "keep your elbows straight"
+                    pose_status = "Keep your elbows straight"
                 elif not 175 < back_angle_l < 200 and not 175 < back_angle_r < 200:
-                    pose_status = "activate your core and straighten your back"
+                    pose_status = "Activate your core and straighten your back"
                 elif not 170 < leg_angle_l < 195 and not 170 < leg_angle_r < 190:
-                    pose_status = "keep your legs straight"
+                    pose_status = "Keep your legs straight"
                 else:
                     pose_status = "Perfect form ! Keep it up!"
 
                 current_time = time.time()
                 if pose_status != previous_pose and (current_time - last_spoken_time) > cooldown:
                     threading.Thread(target=speak, args=(pose_status,), daemon=True).start()
+                    print("Feedback : ", pose_status)
                     previous_pose = pose_status
                     last_spoken_time = current_time
+
+                if "Perfect" in pose_status:
+                    if correct_pose_start_time is None:
+                        correct_pose_start_time = current_time
+                else:
+                    if correct_pose_start_time is not None:
+                        total_correct_time += current_time - correct_pose_start_time
+                        correct_pose_start_time = None
 
                 all_angles = [hand_angle_l, back_angle_l, leg_angle_l, hand_angle_r, back_angle_r, leg_angle_r]
                 ideal_angles = [180, 185, 180, 180, 185, 180]
@@ -184,6 +224,17 @@ def handstand(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose
                 cv2.putText(image, f"Accuracy: {avg_accuracy:.2f}%", (w - 250, 50),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 215, 0), 2)
 
+                elapsed_time = int(time.time() - start_time)
+                correct_time_display = int(total_correct_time) if correct_pose_start_time is None else int(total_correct_time + (time.time() - correct_pose_start_time))
+
+                cv2.putText(image, f"Total Time: {elapsed_time}s", (10, h - 80),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+                cv2.putText(image, f"Correct Time: {correct_time_display}s", (10, h - 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+                cv2.putText(image, pose_status, (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                         mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=2),
                                         mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2))
@@ -192,9 +243,15 @@ def handstand(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
+    cap.release()
+    cv2.destroyAllWindows()
+    print(f"Total time: {int(time.time() - start_time)} seconds")
+    print(f"Total time with correct pose: {int(total_correct_time) if correct_pose_start_time is None else int(total_correct_time + (time.time() - correct_pose_start_time))} seconds")
+
+
 # ---------------------------------------------------------------------------------------------------
 
-def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose):
+def frontlever(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time):
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -214,7 +271,7 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
                 def get_point(landmark):
                     return [landmarks[landmark.value].x, landmarks[landmark.value].y]
 
-                l_ear = get_point(mp.pose.PoseLandmark.LEFT_EAR)
+                l_ear = get_point(mp_pose.PoseLandmark.LEFT_EAR)
                 l_shoulder = get_point(mp_pose.PoseLandmark.LEFT_SHOULDER)
                 l_wrist = get_point(mp_pose.PoseLandmark.LEFT_WRIST)
                 l_elbow = get_point(mp_pose.PoseLandmark.LEFT_ELBOW)
@@ -222,7 +279,7 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
                 l_knee = get_point(mp_pose.PoseLandmark.LEFT_KNEE)
                 l_ankle = get_point(mp_pose.PoseLandmark.LEFT_ANKLE)
 
-                r_ear = get_point(mp.pose.PoseLandmark.RIGHT_EAR)
+                r_ear = get_point(mp_pose.PoseLandmark.RIGHT_EAR)
                 r_wrist = get_point(mp_pose.PoseLandmark.RIGHT_WRIST)
                 r_elbow = get_point(mp_pose.PoseLandmark.RIGHT_ELBOW)
                 r_shoulder = get_point(mp_pose.PoseLandmark.RIGHT_SHOULDER)
@@ -247,7 +304,7 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
                     pose_status = "raise your hips"
                 elif not 30 < shoulder_angle_l < 60 and not 30 < shoulder_angle_r < 60:
                     pose_status = "raise your hips up and lock your scapula"
-                elif not 175 < head_angle_l < 185 and not 175 < head_angle_r < 185:
+                elif not 160 < head_angle_l < 185 and not 160 < head_angle_r < 185:
                     pose_status = "keep your head and neck straight"
                 elif not 170 < leg_angle_l < 195 and not 170 < leg_angle_r < 190:
                     pose_status = "keep your legs straight"
@@ -257,11 +314,20 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
                 current_time = time.time()
                 if pose_status != previous_pose and (current_time - last_spoken_time) > cooldown:
                     threading.Thread(target=speak, args=(pose_status,), daemon=True).start()
+                    print("Feedback : ", pose_status)
                     previous_pose = pose_status
                     last_spoken_time = current_time
 
-                all_angles = [hand_angle_l, back_angle_l, head_angle_r, leg_angle_l, hand_angle_r, head_angle_r, back_angle_r, leg_angle_r]
-                ideal_angles = [180, 90, 180, 180, 180, 180, 90, 180]
+                if "Perfect" in pose_status:
+                    if correct_pose_start_time is None:
+                        correct_pose_start_time = current_time
+                else:
+                    if correct_pose_start_time is not None:
+                        total_correct_time += current_time - correct_pose_start_time
+                        correct_pose_start_time = None
+
+                all_angles = [hand_angle_l, back_angle_l, head_angle_l, shoulder_angle_l, leg_angle_l, hand_angle_r, head_angle_r, back_angle_r, shoulder_angle_r, leg_angle_r]
+                ideal_angles = [180, 180, 180, 45, 180, 180, 180, 180, 45, 180]
                 errors = [abs(a - b) for a, b in zip(ideal_angles, all_angles)]
                 accuracies = [max(0, 100 - (e / 30) * 100) for e in errors]
                 avg_accuracy = sum(accuracies) / len(accuracies)
@@ -277,6 +343,18 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
                 cv2.putText(image, f"Accuracy: {avg_accuracy:.2f}%", (w - 250, 50),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 215, 0), 2)
 
+                elapsed_time = int(time.time() - start_time)
+                correct_time_display = int(total_correct_time) if correct_pose_start_time is None else int(
+                    total_correct_time + (time.time() - correct_pose_start_time))
+
+                cv2.putText(image, f"Total Time: {elapsed_time}s", (10, h - 80),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+                cv2.putText(image, f"Correct Time: {correct_time_display}s", (10, h - 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+                cv2.putText(image, pose_status, (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                         mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=2),
                                         mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2))
@@ -285,15 +363,22 @@ def frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pos
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
+    cap.release()
+    cv2.destroyAllWindows()
+    print(f"Total time: {int(time.time() - start_time)} seconds")
+    print(
+        f"Total time with correct pose: {int(total_correct_time) if correct_pose_start_time is None else int(total_correct_time + (time.time() - correct_pose_start_time))} seconds")
+
+
 while True:
     n = input("Please select a pose : ")
     n.lower()
     if n == "lsit":
-        lsit(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose)
+        lsit(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time)
     elif n == "handstand":
-        handstand(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose)
+        handstand(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time)
     elif n == "frontlever":
-        frontlever(cap, cooldown, last_spoken_time, engine, speak_lock, previous_pose, mp_drawing, mp_pose)
+        frontlever(cap, cooldown, last_spoken_time, previous_pose, mp_drawing, mp_pose, start_time, correct_pose_start_time, total_correct_time)
     else:
         break
 # ---------------------------------------------------------------------------------------------------
